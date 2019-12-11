bf9e29997bfa668f0236b32ee58e63b04759d650
kai-haskell <me@kaihaskell.com>
2019-12-11 16:23:09 -0500

    Setup

---
+++ src/reducers/reducers.js
@@ -0,0 +1,10 @@
+export const initialState = () => {};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return {
+        ...state
+      };
+  }
+};
